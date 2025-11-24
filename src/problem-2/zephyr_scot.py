ee73fefe2e288e27cb2d986d0d5aae7b1a5cf6a6
Kenneth <Kenneth012004@outlook.com>
2025-11-24 17:02:49 -0500

    Coverage reports

--- src/problem-2/zephyr_scot.py
+++ src/problem-2/zephyr_scot.py
@@ -4,7 +4,7 @@ def Solve(envelopes):
     maxEnvelopes = 1
     for I in range(1, len(envelopes)):
         for j in range(I):
-            if envelopes[j][0] < envelopes[I][0] and envelopes[j][1] < envelopes[I][1]:
+            if envelopes[j][0] <= envelopes[I][0] and envelopes[j][1] < envelopes[I][1]:
                 maxEnvelopes = max(maxEnvelopes, Solve(envelopes[j:I+1]) + 1)
         maxEnvelopes = max(maxEnvelopes, Solve(envelopes[I:I+1]) + 1)
     return maxEnvelopes
